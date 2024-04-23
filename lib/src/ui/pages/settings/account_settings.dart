0cd05e0fd1f8dd87909bc8ff4b8089afcc66bf69
92176099 <65901079+NPCuong069@users.noreply.github.com>
2024-04-23 13:58:40 +0700

    add logout

--- lib/src/ui/pages/settings/account_settings.dart
+++ lib/src/ui/pages/settings/account_settings.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:habitpunk/src/storage/SecureStorage.dart';
+import 'package:habitpunk/src/ui/pages/login_page.dart';
 
 class AccountSettingsPage extends StatelessWidget {
   @override
@@ -12,19 +14,25 @@ class AccountSettingsPage extends StatelessWidget {
       body: ListView(
         children: [
           _buildSectionHeader('ACCOUNT INFO'),
-          _buildListTile(context, 'Username', 'Piraka', trailing: Icon(Icons.edit, color: Colors.white)),
+          _buildListTile(context, 'Username', 'Piraka',
+              trailing: Icon(Icons.edit, color: Colors.white)),
           _buildListTile(context, 'Email', 'npc06999@gmail.com'),
           _buildSectionHeader('LOGIN METHODS'),
-          _buildListTile(context, 'Password', 'Not Set', trailing: ElevatedButton(onPressed: () {}, child: Text('Add Password'))),
-          _buildListTile(context, 'Google', 'Not Set', trailing: OutlinedButton(onPressed: () {}, child: Text('Connect'))),
-          _buildListTile(context, 'Apple', 'npc06999@gmail.com', trailing: TextButton(onPressed: () {}, child: Text('Disconnect', style: TextStyle(color: Colors.red)))),      
+          _buildListTile(context, 'Password', 'Not Set',
+              trailing: ElevatedButton(
+                  onPressed: () {}, child: Text('Add Password'))),
+          _buildListTile(context, 'Google', 'Not Set',
+              trailing:
+                  OutlinedButton(onPressed: () {}, child: Text('Connect'))),
+          _buildListTile(context, 'Apple', 'npc06999@gmail.com',
+              trailing: TextButton(
+                  onPressed: () {},
+                  child:
+                      Text('Disconnect', style: TextStyle(color: Colors.red)))),
           _buildSectionHeader('Quit The Game?'),
-      
           ListTile(
             title: Text('Log Out', style: TextStyle(color: Colors.red)),
-            onTap: () {
-              // Handle to log out the account
-            },
+            onTap: () => _logout(context),
           ),
         ],
       ),
@@ -34,18 +42,30 @@ class AccountSettingsPage extends StatelessWidget {
   Widget _buildSectionHeader(String title) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-      child: Text(title, style: TextStyle(color: Colors.grey, fontWeight: FontWeight.bold)),
+      child: Text(title,
+          style: TextStyle(color: Colors.grey, fontWeight: FontWeight.bold)),
     );
   }
 
-  Widget _buildListTile(BuildContext context, String title, String subtitle, {Widget? trailing, bool isNavigation = false}) {
+  Widget _buildListTile(BuildContext context, String title, String subtitle,
+      {Widget? trailing, bool isNavigation = false}) {
     return ListTile(
       title: Text(title, style: TextStyle(color: Colors.white)),
       subtitle: Text(subtitle, style: TextStyle(color: Colors.white70)),
       trailing: trailing,
-      onTap: isNavigation ? () {
-        // Handle navigation
-      } : null,
+      onTap: isNavigation
+          ? () {
+              // Handle navigation
+            }
+          : null,
+    );
+  }
+
+  void _logout(BuildContext context) async {
+    await SecureStorage().deleteSecureData('jwt'); // Delete the JWT token
+    Navigator.of(context, rootNavigator: true).pushAndRemoveUntil(
+      MaterialPageRoute(builder: (context) => LoginPage()),
+      (Route<dynamic> route) => false,
     );
   }
 }
